8832528130c3b930d10caeb3e73d6dd88ae07d09
zkjpweqrcq <97687399+zkjpweqrcq@users.noreply.github.com>
2022-12-02 23:06:02 +0200

    fixed selector

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('#score--1');
+const score1El = document.getElementById('score--1');
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
 
